66d4521910875c40caf4c825d50b500302dd4469
shawn <shawnviera741@gmail.com>
2025-01-15 17:05:27 -0500

    Week 2 Lab

---
+++ COMP2139-ICE/Controllers/ProjectController.cs
@@ -0,0 +1,37 @@
+﻿using COMP2139_ICE.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2139_ICE.Controllers;
+
+public class ProjectController : Controller
+{
+  [HttpGet] // 
+  public IActionResult Index()
+  {
+   var projects = new List<Project>();
+   {
+       new Project {ProjectId = 1, Name = "Project 1", Description = "My First Project 1"};
+   }
+   return View(projects);
+  }
+
+  [HttpGet]    
+  public IActionResult Create()
+  {
+      return View();
+  }
+  
+  
+  [HttpPost]    
+  public IActionResult Create(Project project)
+  {
+      return View();
+  }
+  
+}
+
+
+
+
+
+
---
+++ COMP2139-ICE/Models/Project.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP2139_ICE.Models;
+
+public class Project
+{
+    /// <summary>
+    /// The unique primary key for projects
+    /// </summary>
+    public int ProjectId { get; set; }
+    /// <summary>
+    /// The name of the project
+    /// Required - Ensures the property must be provided (must have a value)
+    /// </summary>
+    
+    
+    [Required]
+    public required string Name { get; set; }
+    
+    
+    public string? Description { get; set; }/// can be null or not null
+    
+    [DataType(DataType.Date)]
+    public DateTime StartDate { get; set; }
+    
+    [DataType(DataType.Date)]
+    public DateTime EndDate { get; set; }
+    
+    public string? Status { get; set; }
+    
+    
+    
+}
